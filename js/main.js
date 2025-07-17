4ad848757f1691950b7c908c5c224026c3fadc4b
CoolBoy376 <74808220+CoolBoy376@users.noreply.github.com>
2025-07-17 16:16:33 +1000

    Update main.js
    Adding function to append safe search variable to Search for on google hyperlink

--- js/main.js
+++ js/main.js
@@ -10,10 +10,17 @@
 		}
 	}
 
+	function enableSafeSearch() {
+	    const p = document.getElementsByClassName("gcsc-more-maybe-branding-root")[0];
+	    const a = p.children[0];
+	    a.setAttribute("href", `${a.getAttribute("href")}&safe=strict`);
+	}
+
 	setTimeout(function(){
 		const t = document.getElementById("___gcse_0");
 		const o = new MutationObserver(()=>{
 			loadTitle();
+			enableSafeSearch();
 		});
 		
 		o.observe(t, {
@@ -21,7 +28,12 @@
 			subtree: true
 		});
 	}, 2000);
+
+	function init() {
+		loadTitle();
+		enableSafeSearch();
+	}
 	
-	window.addEventListener('hashchange',()=>{loadTitle();});
-	window.addEventListener('DOMContentLoaded',()=>loadTitle(););
+	window.addEventListener('hashchange',()=>{init();});
+	window.addEventListener('DOMContentLoaded',()=>init(););
 })();
