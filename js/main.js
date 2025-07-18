35d37e7a92a290583acbf446491c68585e9a4f47
CoolBoy376 <74808220+CoolBoy376@users.noreply.github.com>
2025-07-18 14:56:49 +1000

    Removed log
    I know the code isn't clean, but debugging for phones is a pain

--- js/main.js
+++ js/main.js
@@ -1,10 +1,4 @@
 (function(){
-function log(msg) {
-	const d = document.getElementById("log");
-	const p = document.createElement("p");
-p.innerText = `Log: ${msg}`;
-d.appendChild(p);
-}
 	function loadTitle() {
 		if (window.location.href.includes('#')) {
 			const h = window.location.hash.substring(1);
@@ -17,35 +11,25 @@ d.appendChild(p);
 
 	var st=null;
 	function enableSafeSearch() {
-		log("waiting for intrustions");
 try {
-log("Function was called");
-	
 	    var p = document.getElementsByClassName("gcsc-more-maybe-branding-root")[0];
 	if (p==null)
 		p = document.getElementsByClassName("gcsc-more-maybe-branding-box")[0];
 	
 		if (p!=null) {
-log("p not null");
 		    const a = p.children[0];
 			if (a!=null) {
-log("a not null");
 				const url = `${a.getAttribute("href")}&safe=strict`;
 		    		a.setAttribute("href", url);
 clearInterval(st);
 			}
-		} else {
-			log("p is empty. Why?");
-			log(`p value: ${p}`);
 		}
 } catch(e) {
 console.error(e);
-log(`Error Message: ${e}`);
 }
 	}
 
 	function startTimer() {
-		log("timer called");
 	st = setInterval(function(){enableSafeSearch();}, 1000);
 	}
 
