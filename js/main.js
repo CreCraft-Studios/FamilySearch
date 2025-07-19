4f08219c1f72c74df3b794737b87f377d4afd639
CoolBoy376 <74808220+CoolBoy376@users.noreply.github.com>
2025-07-19 17:53:05 +1000

    Temp logging

--- js/main.js
+++ js/main.js
@@ -1,28 +1,48 @@
 (function(){
 var safeApplied=false;
+function log(log) {
+	const l = document.getElementById("log");
+	const m = document.createElement("p")
+m.innerText = `Log: ${msg}`;
+l.appendChild(m);
+}
+	
 function needToRejectSearch(query) {
-	if (query.includes("porn") || query.includes("xnxx") || query.includes("xvideos")) {
-		const d = document.getElementsByClassName("gsc-expansionArea")[0];
-		if (d!=null) {
-			if (d.children.length<0) {
-				setTimeout(function(){needToRejectSearch(query);}, 1000);
-			} else {
-				while (d.children.length>0) {
-				    d.removeChild(d.firstChild);
+	log("got called");
+	try {
+		if (query.includes("porn") || query.includes("xnxx") || query.includes("xvideos")) {
+			log("found. Need to delete");
+			const d = document.getElementsByClassName("gsc-expansionArea")[0];
+			if (d!=null) {
+				log("d found. Time to delete");
+				if (d.children.length<0) {
+					log("no children");
+					setTimeout(function(){needToRejectSearch(query);}, 1000);
+				} else {
+					log("has children. Deleting");
+					while (d.children.length>0) {
+					    d.removeChild(d.firstChild);
+					}
+					const p = document.createElement("div");
+					p.setAttribute("class", "gsc-webResult gsc-result");
+					const p2 = document.createElement("div");
+					p2.setAttribute("class", "gs-webResult gs-result gs-no-results-result");
+					const p3 = document.createElement("div");
+					p3.setAttribute("class", "gs-snippet");
+					p3.innerText = "No Results Found. (Could be due to restrictions)";
+					
+					p2.appendChild(p3);
+					p.appendChild(p2);
+					d.appendChild(p);
 				}
-				const p = document.createElement("div");
-				p.setAttribute("class", "gsc-webResult gsc-result");
-				const p2 = document.createElement("div");
-				p2.setAttribute("class", "gs-webResult gs-result gs-no-results-result");
-				const p3 = document.createElement("div");
-				p3.setAttribute("class", "gs-snippet");
-				p3.innerText = "No Results Found. (Could be due to restrictions)";
-				
-				p2.appendChild(p3);
-				p.appendChild(p2);
-				d.appendChild(p);
+			} else {
+				log("d is null. trying again");
+				setTimeout(function(){needToRejectSearch(query);}, 1000);
 			}
-		} else setTimeout(function(){needToRejectSearch(query);}, 1000);
+		}
+	} catch(e) {
+		console.error(e);
+		log(e);
 	}
 }
 	
